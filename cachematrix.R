d4a55997a30735141784cba32393365be7cbc4e4
Lilly Chin <lchin@coursera.org>
2014-07-24 14:28:37 -0700

    Finish assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create matrix wrapper that can cache its own inverse
+## This saves computation time, so we don't have to constantly recompute the inverse
 
-## Write a short comment describing this function
-
+## Create matrix that can cache its own inverse
+## Has function
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(value) inverse <<- value
+  getInverse <- function() inverse
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## Function that calculates inverse of matrix
+## Only recomputes inverse if value not already cached
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if(is.null(inverse)) {
+    message("recomputing data")
+    inverse <- solve(x$get())
+    x$setInverse(inverse)
+  }
+  inverse
 }
